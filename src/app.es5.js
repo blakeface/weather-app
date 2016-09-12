d20f4cb5b6023821c9fbd5492b8d1552c9015080
Blake Netzeband <blake.netzeband@gmail.com>
2016-09-12 11:32:07 -0600

    unfactored temp algorithm complete. append to DOM

--- src/app.es5.js
+++ src/app.es5.js
@@ -3,6 +3,28 @@
 var APIKEY = '6e1fc93970e4f39c34301ff3e1a16977';
 var city = 'Boulder';
 
-$.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&APPID=' + APIKEY).then(function (results) {
-  return console.log('results:', results);
+$('.root').append('<h1>Weather Forecast for ' + city + '</h1>\n  <div class="weather-details"></div>');
+
+$.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&APPID=' + APIKEY, successCB).fail(function (err) {
+  return console.log('err:', err);
 });
+
+function successCB(data) {
+  console.log(data);
+  var today = data.list[0].dt_txt.slice(0, 10);
+  var todayData = [];
+  var todayTemp = 0;
+
+  for (var i = 0; i < data.list.length; i++) {
+    if (data.list[i].dt_txt.slice(0, 10) == today) todayData.push(data.list[i]);
+  }
+
+  for (var j = 0; j < todayData.length; j++) {
+    todayTemp += todayData[j].main.temp;
+  }
+  todayTemp = todayTemp / j;
+
+  console.log(todayTemp);
+
+  $('.weather-details').append('<p>Temperature: ' + todayTemp + '&degF</p>');
+}
--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 const APIKEY = '6e1fc93970e4f39c34301ff3e1a16977';
 const city = 'Boulder';
 
-$.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${APIKEY}`)
-.then( results => console.log('results:',results) );
+$('.root').append(
+  `<h1>Weather Forecast for ${city}</h1>
+  <div class="weather-details"></div>`
+);
+
+$.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&APPID=${APIKEY}`, successCB)
+.fail( err => console.log('err:',err) );
+
+function successCB(data) {
+  console.log(data);
+  let today = data.list[0].dt_txt.slice(0,10);
+  let todayData = [];
+  let todayTemp = 0;
+
+  for (var i = 0; i < data.list.length; i++) {
+    if (data.list[i].dt_txt.slice(0,10) == today)
+      todayData.push(data.list[i]);
+  }
+
+  for (var j = 0; j < todayData.length; j++) {
+    todayTemp += todayData[j].main.temp;
+  }
+  todayTemp = todayTemp / j;
+
+  console.log(todayTemp);
+
+  $('.weather-details').append(
+    `<p>Temperature: ${todayTemp}&degF</p>`
+  );
+}
